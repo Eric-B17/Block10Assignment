0b57421ed6e1b758d966a334153c6acc052cc9ab
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-02-05 12:07:37 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const pipe = (...functions) => input => functions.reduce((acc, fn) => fn(acc), input);
+const increment = x => x + 1;
+const doubleValue = x => x * 2;
+
+const processNumber = pipe(increment, doubleValue);
+console.log(processNumber(3)); // Output: 8
+
+const squareValue = x => x * x;
+const halveValue =  x => x / 2;
+
+const processNumber2 = pipe(squareValue, halveValue);
+console.log(processNumber2(3));
+//
